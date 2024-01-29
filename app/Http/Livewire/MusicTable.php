9bdf52f3488faa7b0564852eed70182c33f16ef8
kolydart <kolydart@macbeth.gateweb.gr>
2024-01-29 12:43:23 +0200

    MusicTable component

---
+++ app/Http/Livewire/MusicTable.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use PowerComponents\LivewirePowerGrid\Rules\{Rule, RuleActions};
+use PowerComponents\LivewirePowerGrid\Traits\{ActionButton, WithExport};
+use PowerComponents\LivewirePowerGrid\Filters\Filter;
+use PowerComponents\LivewirePowerGrid\{Button, Column, Exportable, Footer, Header, PowerGrid, PowerGridComponent, PowerGridColumns};
+
+final class MusicTable extends PowerGridComponent
+{
+    use ActionButton;
+    use WithExport;
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Features Setup
+    |--------------------------------------------------------------------------
+    | Setup Table's general features
+    |
+    */
+    public function setUp(): array
+    {
+        $this->showCheckBox();
+
+        return [
+            Exportable::make('export')
+                ->striped()
+                ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
+            Header::make()->showSearchInput(),
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Datasource
+    |--------------------------------------------------------------------------
+    | Provides data to your Table using a Model or Collection
+    |
+    */
+
+    /**
+     * PowerGrid datasource.
+     *
+     * @return Builder<\App\User>
+     */
+    public function datasource(): Builder
+    {
+        return User::query();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Relationship Search
+    |--------------------------------------------------------------------------
+    | Configure here relationships to be used by the Search and Table Filters.
+    |
+    */
+
+    /**
+     * Relationship search.
+     *
+     * @return array<string, array<int, string>>
+     */
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Add Column
+    |--------------------------------------------------------------------------
+    | Make Datasource fields available to be used as columns.
+    | You can pass a closure to transform/modify the data.
+    |
+    | ❗ IMPORTANT: When using closures, you must escape any value coming from
+    |    the database using the `e()` Laravel Helper function.
+    |
+    */
+    public function addColumns(): PowerGridColumns
+    {
+        return PowerGrid::columns()
+            ->addColumn('id')
+            ->addColumn('name')
+
+           /** Example of custom column using a closure **/
+            ->addColumn('name_lower', fn (User $model) => strtolower(e($model->name)))
+
+            ->addColumn('songs',fn ($model) => json_encode($model->songs))
+            ->addColumn('email')
+            ->addColumn('locale')
+            ->addColumn('created_at_formatted', fn (User $model) => Carbon::parse($model->created_at)->format('d/m/Y H:i:s'));
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Include Columns
+    |--------------------------------------------------------------------------
+    | Include the columns added columns, making them visible on the Table.
+    | Each column can be configured with properties, filters, actions...
+    |
+    */
+
+     /**
+      * PowerGrid Columns.
+      *
+      * @return array<int, Column>
+      */
+    public function columns(): array
+    {
+        return [
+            Column::make('Id', 'id'),
+            Column::make('Name', 'name')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Songs', 'songs', 'songs')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Email', 'email')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Locale', 'locale')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('Created at', 'created_at_formatted', 'created_at')
+                ->sortable(),
+
+        ];
+    }
+
+    /**
+     * PowerGrid Filters.
+     *
+     * @return array<int, Filter>
+     */
+    public function filters(): array
+    {
+        return [
+            Filter::inputText('name')->operators(['contains']),
+            Filter::inputText('songs')->operators(['contains']),
+            Filter::inputText('email')->operators(['contains']),
+            Filter::inputText('locale')->operators(['contains']),
+            Filter::datetimepicker('created_at'),
+        ];
+    }
+
+    public function beforeSearch(string $field = null, string $search = null)
+    {
+        dd('it works!');
+
+        if ($field === 'songs') {
+            // Encode to Unicode and remove json enclosing.
+            $search = trim(ltrim(rtrim(json_encode([$search]), '"]'), '["'));
+
+            //Triple escape backlashes
+            $search = preg_replace('/\\\\/', '\\\\\\\\\\', $search);
+
+            return $search;
+        }
+
+        return $search;
+    }
+ }
